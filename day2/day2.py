f997f21bb2809222df4c43aa9ff1f5b037ac5f9f
Grégoire Meylan <it@propulse-lab.com>
2018-12-21 21:57:11 +0100

    day2 part 1

--- day2/day2.py
+++ day2/day2.py
@@ -1,6 +1,23 @@
-sum = 0
+exactly_two = 0
+exactly_three = 0
 with open('input') as file:
     for line in file:
-        
+        seen_two_times = False
+        seen_three_times = False
+        character_set = ''.join(set(line.rstrip()))
+        print('character_set', repr(character_set))
+        for character in character_set:
 
+            print('character', character)
+            number = line.count(character)
+            if number == 2 and not seen_two_times:
+                seen_two_times = True
+                print('found 2 times in:', character, line)
+                exactly_two += 1
+            if number ==  3 and not seen_three_times:
+                seen_three_times = True
+                print('found 3 times in:', character, line)
+                exactly_three += 1
+print(exactly_three, exactly_two)  
+sum = exactly_three * exactly_two
 print(sum)
